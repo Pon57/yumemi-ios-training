d5b6d27223ea2248bd6576f5666e816179337e9e
Pon <tyanpon57@gmail.com>
2021-07-31 19:42:07 +0900

    init xcode project

---
+++ yumemi-ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi-ios-training
+//
+//  Created by Pon on 2021/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
